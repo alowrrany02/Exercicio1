16520d484cf295c5a2489f3d9bae5fa9797ae64a
alowrrany02 <130370163+alowrrany02@users.noreply.github.com>
2025-03-25 20:20:51 -0300

    up

---
+++ questao2.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+
+atual = 2025
+futuro = 2030
+
+idade_2030 = idade + (futuro - atual)
+
+print(f"Olá {nome}, sua idade atual é {idade} e em 2030 vc terá {idade_2030} anos de idade!")
