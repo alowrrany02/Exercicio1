0fe5af045e15428b87eff7ed0b6179e44a48adb7
alowrrany02 <130370163+alowrrany02@users.noreply.github.com>
2025-05-07 21:40:48 -0300

    Up

--- parouimpar.py
+++ parouimpar.py
@@ -1,21 +1,38 @@
-while True:
-    try: 
-        num = int(input("Digite um numero:"))
-        if (num % 2 == 0):
-            print(f"O numero {num} é par")
-        else:
-            print(f"O o numero {num} é impar")
-    except ValueError: 
-       print("Digite um numero valido")
-       continue 
-    
+def veri_paridade(num):
+    if num % 2 == 0:
+        print(f"O numero {num} é par")
+    else:
+        print(f"O o numero {num} é impar")
+
+def pedir_num ():
     while True:
-        resposta = input("Voce quer continuar?: (s/n)").strip().lower()
+        entrada = input("Digite um numero inteiro:")
+        try:
+            num = int(entrada)
+            return num
+        except ValueError:
+            print("Por favor digite um numero válido!")
+
+def pedir_continuar():
+    while True:
+        resposta = input("Voce deseja continuar?(s/n)").strip().lower()
         if resposta == 's':
-            break
+            return True
         elif resposta == 'n':
-            print('Pronto fechei!')
-            exit()
+            return False
         else:
-            print("Por favor digite somente s ou n")
-            
+            print("Por favor, digite s para continuar ou n para parar!")
+
+
+def main():
+    print("=== Teste de Paridade ===")
+    while True:
+        num = pedir_num()
+        resposta = veri_paridade(num)
+        print(resposta)
+
+        if not pedir_continuar():
+            print("Pronto fechei")
+            break
+if __name__ == "__main__":
+    main()
